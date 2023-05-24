eb33e91bc57021723978815933b05b7a4c52886b
IT18002720_Dilan_V <it18002720@my.sliit.lk>
2023-05-24 11:25:39 +0530

    jwt token and password hasing

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require("cors");
 const connectDB = require("./config/db");
 const User = require("./models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs');
+
 //Middleware
 app.use(cors());
 app.use(express.json());
@@ -11,17 +14,21 @@ connectDB();
 //register -Q8XnemStVHpe4Wll
 app.post("/api/register", async (req, res) => {
   try {
+    const password = req.body.password
+    // Generate a salt and hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
     await User.create({
       user_id: req.body.user_id,
       user_name: req.body.user_name,
-      password: req.body.password,
+      password: hashedPassword,
       email: req.body.email,
       athar_id: req.body.athar_id,
       phone_number: req.body.phone_number,
       category: req.body.category, // admin, service, rental and normal
       category_dis: req.body.category_dis,
     });
-    res.json({ status: "ok" });
+    res.json({ status: "Registration successful" });
   } catch (err) {
     console.log(err);
     res.json({ status: "error", error: "Duplicate Email!" });
@@ -29,19 +36,36 @@ app.post("/api/register", async (req, res) => {
 });
 
 app.post("/api/login", async (req, res) => {
+  // const token = req.headers['x-access-token']
+  // try{
+  //   const decoded = jwt.verify(token, 'hdfsajkfhlsdkja')
+  //   const email = decoded.email
+  // }catch(err){
+  //   console.log(err)
+  //   res.json({status: 'error', error:'invalid token'})
+  // }
   const user = await User.findOne({
     user_name: req.body.user_name,
     password: req.body.password,
   });
 
   if (user) {
-    return res.json({ status: "ok", user: true });
+    const token = jwt.sign(
+      {
+        name: user._id,
+        email: user.email,
+      },
+      'hdfsajkfhlsdkja'
+    );
+     // persist the token as 't' in cookie with expiry date
+     res.cookie("t", token, { expire: new Date() + 9999 });
+    return res.json({ status: "User Successfully login", user: token });
   } else {
     return res.json({ status: "error", user: false });
   }
 
-  res.json({ status: "User successfully register " });
 });
+
 app.listen(1337, () => {
   console.log("server started from 1337");
 });
